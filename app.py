b21b038bf0231811c8a9e2c649c867cfa731b6e5
parinitapachalla <85706983+parinitapachalla@users.noreply.github.com>
2021-06-10 16:52:03 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "UTA ID: 1001831207   Name: Sai Parinita Pachalla"
+    return render_template('index.html')
